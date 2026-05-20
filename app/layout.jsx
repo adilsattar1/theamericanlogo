08754e5bab8e5f2c58c901df5f201edf40f6b6e8
Nexgendesigners01 <nexgendesigners01@gmail.com>
2026-05-20 21:21:06 +0500

    clarity added

--- app/layout.jsx
+++ app/layout.jsx
@@ -45,6 +45,13 @@ export default function RootLayout({ children }) {
           gtag('js', new Date());
           gtag('config', 'G-BCWQTY29JQ');`}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "wu5395k71a");`}
+        </Script>
         <Header />
         <main className="app-container">{children}</main>
         <Footer />
